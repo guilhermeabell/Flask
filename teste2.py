1639f22baf6d6c9bcff6415e079b45555035e3a9
guilhermeabel99 <gpeuvis@gmail.com>
2021-02-07 21:52:46 -0300

    range flask

--- teste2.py
+++ teste2.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return render_template("index.html", content=name, r=2)
+	return render_template("index.html", content=["tim", "joe", "bill"])
 
 		if __name__ == '__main__':
 			app.run()
